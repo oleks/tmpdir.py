c592dcb67ff6615c7f123b004b1e45f84a92fa50
oleks <oleks@oleks.info>
2017-02-23 14:41:15 +0100

    Use flake8

--- tmpdir.py
+++ tmpdir.py
@@ -8,7 +8,7 @@
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
 #
@@ -20,109 +20,137 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import argparse, os, os.path, shutil, subprocess, sys, tempfile
+import argparse
+import os
+import os.path
+import shutil
+import subprocess
+import sys
+import tempfile
+
 
 # http://stackoverflow.com/a/12514470/5801152
 def copytree(src, dst, symlinks=False, ignore=None):
-  for item in os.listdir(src):
-    s = os.path.join(src, item)
-    d = os.path.join(dst, item)
-    if os.path.isdir(s):
-      shutil.copytree(s, d, symlinks, ignore)
-    else:
-      shutil.copy2(s, d)
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
 
 def do_with_tmpdir(args, tmpdir):
 
-  if args.copy:
-    if os.path.isfile(args.copy):
-      shutil.copy2(args.copy, tmpdir)
-    else:
-      copytree(args.copy, tmpdir)
+    if args.copy:
+        if os.path.isfile(args.copy):
+            shutil.copy2(args.copy, tmpdir)
+        else:
+            copytree(args.copy, tmpdir)
 
-  cwd = "."
+    cwd = "."
 
-  had_tmpdir = False
-  for i, arg in enumerate(args.args):
-    if arg == "%%TMPDIR":
-      args.args[i] = tmpdir
-      had_tmpdir = True
+    had_tmpdir = False
+    for i, arg in enumerate(args.args):
+        if arg == "%%TMPDIR":
+            args.args[i] = tmpdir
+            had_tmpdir = True
 
-  if (args.cwd or not had_tmpdir) and not args.keepwd:
-    cwd = tmpdir
+    if (args.cwd or not had_tmpdir) and not args.keepwd:
+        cwd = tmpdir
 
-  env = os.environ.copy()
-  if args.env:
-    env["TMPDIR"] = tmpdir
+    env = os.environ.copy()
+    if args.env:
+        env["TMPDIR"] = tmpdir
 
-  command = [args.command] + args.args
+    command = [args.command] + args.args
 
-  proc = subprocess.Popen(command, cwd=cwd, env=env)
-  return proc.wait()
+    proc = subprocess.Popen(command, cwd=cwd, env=env)
+    return proc.wait()
+
 
 def main_with_args(args):
-  dir=tempfile.gettempdir()
-  if args.dir:
-    dir=args.dir
+    dir = tempfile.gettempdir()
+    if args.dir:
+        dir = args.dir
 
-  prefix=tempfile.gettempprefix()
-  if args.prefix:
-    prefix=args.prefix
+    prefix = tempfile.gettempprefix()
+    if args.prefix:
+        prefix = args.prefix
 
-  suffix=""
-  if args.suffix:
-    suffix=args.suffix
+    suffix = ""
+    if args.suffix:
+        suffix = args.suffix
 
-  retval = 0
-  tmpdir = tempfile.mkdtemp(
-    dir=dir, prefix=prefix, suffix=suffix)
-  try:
-    retval = do_with_tmpdir(args, tmpdir)
-  finally:
-    shutil.rmtree(tmpdir)
+    retval = 0
+    tmpdir = tempfile.mkdtemp(
+        dir=dir, prefix=prefix, suffix=suffix)
+    try:
+        retval = do_with_tmpdir(args, tmpdir)
+    finally:
+        shutil.rmtree(tmpdir)
 
-  return retval
+    return retval
+
 
 def main():
-  parser = argparse.ArgumentParser(
-    description=
-"""
+    parser = argparse.ArgumentParser(
+        description="""
 Do something with a temporary directory (TMPDIR).
 """)
-  parser.add_argument(
-    "command", metavar="COMMAND",
-    help="the command to execute in the current environment, with the " +
-         "current working directory; if %%TMPDIR does not appear once " +
-         "among its arguments, change working directory to TMPDIR")
-  parser.add_argument(
-    "args", metavar="...", nargs=argparse.REMAINDER,
-    help="arguments to pass to the command; all %%%%TMPDIR arguments " +
-         "will be replaced by the absolute path to the created TMPDIR")
-  parser.add_argument(
-    "-d", "--dir", metavar="PATH",
-    help="create the temporary directory under PATH")
-  parser.add_argument(
-    "-p", "--prefix", metavar="TEXT",
-    help="create the temporary directory with directory name prefix TEXT")
-  parser.add_argument(
-    "-s", "--suffix", metavar="TEXT",
-    help="create the temporary directory with directory name suffix TEXT")
-  parser.add_argument(
-    "-c", "--copy", metavar="PATH",
-    help="copy PATH (recursively) into TMPDIR before running COMMAND")
-  parser.add_argument(
-    "-g", "--cwd", action="store_true",
-    help="cd in to TMPDIR before executing COMMAND")
-  parser.add_argument(
-    "-k", "--keepwd", action="store_true",
-    help="stay in current working directory")
-  parser.add_argument(
-    "-e", "--env", action="store_true",
-    help="set the environment variable TMPDIR to the absolute path to " +
-         "the created tmpdir")
-  args = parser.parse_args()
+    parser.add_argument(
+        "command", metavar="COMMAND",
+        help="""
+the command to execute in the current environment, with the
+current working directory; if %%TMPDIR does not appear once
+among its arguments, change working directory to TMPDIR
+""")
+    parser.add_argument(
+        "args", metavar="...", nargs=argparse.REMAINDER,
+        help="""
+arguments to pass to the command; all %%%%TMPDIR arguments
+will be replaced by the absolute path to the created TMPDIR
+""")
+    parser.add_argument(
+        "-d", "--dir", metavar="PATH",
+        help="""
+create the temporary directory under PATH
+""")
+    parser.add_argument(
+        "-p", "--prefix", metavar="TEXT",
+        help="""
+create the temporary directory with directory name prefix TEXT
+""")
+    parser.add_argument(
+        "-s", "--suffix", metavar="TEXT",
+        help="""
+create the temporary directory with directory name suffix TEXT
+""")
+    parser.add_argument(
+        "-c", "--copy", metavar="PATH",
+        help="""
+copy PATH (recursively) into TMPDIR before running COMMAND
+""")
+    parser.add_argument(
+        "-g", "--cwd", action="store_true",
+        help="""
+cd in to TMPDIR before executing COMMAND
+""")
+    parser.add_argument(
+        "-k", "--keepwd", action="store_true",
+        help="""
+stay in current working directory
+""")
+    parser.add_argument(
+        "-e", "--env", action="store_true",
+        help="""
+set the environment variable TMPDIR to the absolute path to
+the created tmpdir
+""")
+    args = parser.parse_args()
 
-  sys.exit(main_with_args(args))
+    sys.exit(main_with_args(args))
+
 
 if __name__ == "__main__":
-  main()
+    main()
